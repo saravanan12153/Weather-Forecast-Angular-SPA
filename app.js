17b1040a2134d92132b0fbe1b3b0476add43fe80
Sulman Baig <sulman1990@hotmail.com>
2016-02-28 23:38:09 +0500

    Added Routing, controllers and pages

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // My Module
-var weatherApp = angular.module('weatherApp', []);
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+// Routes
+weatherApp.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/home.htm',
+		controller: 'homeController'
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.htm',
+		controller: 'forecastController'
+	})
+});
+
+// Controllers
+weatherApp.controller('homeController', ['$scope', function($scope) {
+	
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+	
+}]);
