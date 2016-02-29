f2dcf24e82f658695a6df9c3a33d7d0a707623ae
Sulman Baig <sulman1990@hotmail.com>
2016-02-29 08:56:13 +0500

    Getting data fro Open API

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 	});
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
 	$scope.city = cityService.city;
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=c1e88be5a69a53f95dda4db23e2b1591", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+	$scope.convertToCelsius = function(degK) {
+		return Math.round(degK - 273.15);
+	}
+	$scope.convertToDate = function(dt) {
+		return new Date(dt * 1000);
+	}
 }]);
