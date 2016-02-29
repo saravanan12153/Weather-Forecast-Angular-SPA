65f454644a1b71421337d9dc50dc33da11bc99bf
Sulman Baig <sulman1990@hotmail.com>
2016-02-29 07:34:23 +0500

    Added custom city service

--- app.js
+++ app.js
@@ -14,11 +14,19 @@ weatherApp.config(function($routeProvider){
 	})
 });
 
+// Services
+weatherApp.service('cityService', function(){
+	this.city = 'New York, NY'
+});
+
 // Controllers
-weatherApp.controller('homeController', ['$scope', function($scope) {
-	
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	});
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-	
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
 }]);
